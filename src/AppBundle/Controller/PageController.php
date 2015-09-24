24d0387340bf71e15c2d5f7a00d725d306cf0b03
ezequiel zacca <ezequielzacca@gmail.com>
2015-09-24 18:52:05 -0300

    commit de prueba

--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -5,7 +5,15 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class PageController extends Controller
 {
+    /**
+     * 
+     * @Route("/prueba")
+     */
+    public function pruebaAction(){
+        return new Response("Prueba Exitosa");
+    }
 }
